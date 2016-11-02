51175dcbf37a0e4c290dbd88502486788ffc2f3c
Daniel Stiner <danstiner@gmail.com>
2016-11-01 23:23:08 -0700

    Test for set 1 challenge 1

--- set1/test/Spec.hs
+++ set1/test/Spec.hs
@@ -1,2 +1,12 @@
+import Test.HUnit
+import Control.Monad (void)
+
+import Lib
+
+test_set1_challenge1 = expected ~=? actual
+  where
+    expected = Right "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
+    actual = hexStringToBase64String "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = void $ runTestTT test_set1_challenge1
