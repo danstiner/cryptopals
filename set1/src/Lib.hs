8c93924c910d40cd1520f37002886bdafab037e0
Daniel Stiner <danstiner@gmail.com>
2016-11-01 23:35:01 -0700

    Set 1 Challenge 2

--- set1/src/Lib.hs
+++ set1/src/Lib.hs
@@ -1,6 +1,8 @@
 module Lib
     ( someFunc
     , hexStringToBase64String
+    , hexStringToBytes
+    , xor
     ) where
 
 import qualified Data.ByteString.Base16 as Base16
@@ -8,6 +10,8 @@ import qualified Data.ByteString.Base64 as Base64
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Char8 as C
 import Data.String
+import Data.Either.Unwrap
+import qualified Data.Bits as Bits
 
 type HexString = String
 type Base64String = String
@@ -15,11 +19,17 @@ type Base64String = String
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
 
+xor :: B.ByteString -> B.ByteString -> B.ByteString
+xor a b = B.pack (B.zipWith Bits.xor a b)
+
 hexStringToBase64String :: HexString -> Either String Base64String
 hexStringToBase64String input = bytesToBase64String <$> hexStringToBytes input
 
 hexStringToBytes :: HexString -> Either String B.ByteString
 hexStringToBytes = base16DecodeCompletely . fromString
+
+hexStringToBytes' :: HexString -> B.ByteString
+hexStringToBytes' = fromRight . hexStringToBytes
 
 bytesToBase64String :: B.ByteString -> Base64String
 bytesToBase64String = C.unpack . Base64.encode
