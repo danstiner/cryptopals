ffdc9ce31363fdf1b373c2c21d5d165fd993e7b6
Daniel Stiner <danstiner@gmail.com>
2016-11-22 01:12:44 -0800

    Better error message

--- set1/src/Lib.hs
+++ set1/src/Lib.hs
@@ -125,7 +125,7 @@ base16DecodeCompletely = toMaybe . Base16.decode
   where
     toMaybe (result, leftovers)
       | B.null leftovers = Right result
-      | otherwise = Left ("Non-base16 input starting at byte " ++ show (B.length result * 2))
+      | otherwise = Left ("Non-base16 input starting at character " ++ show (B.length result * 2))
 
 bruteForceEnglishEncryptedWithSingleByteXOR :: CipherText -> [(PlainText, Key, Score)]
 bruteForceEnglishEncryptedWithSingleByteXOR ciphertext =
