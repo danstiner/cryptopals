d8d36cd7cef7744ef23a540d03e8169582f7121b
Daniel Stiner <danstiner@gmail.com>
2016-11-22 00:43:00 -0800

    Delete extraneous EOL space

--- set1/src/Lib.hs
+++ set1/src/Lib.hs
@@ -213,7 +213,7 @@ test_set1_challenge5 = expected_ciphertext ~=? encryptWithRepeatingKeyXOR key pl
       , "I go crazy when I hear a cymbal"]
     key = C.pack "ICE"
     expected_ciphertext = decodeHex $
-       "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272" ++ 
+       "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272" ++
        "a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f"
 
 test_hammingDistance = C.pack "this is a test" `hammingDistance` C.pack "wokka wokka!!!" ~=? 37
